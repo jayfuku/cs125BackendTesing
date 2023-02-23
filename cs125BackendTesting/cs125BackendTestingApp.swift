fd653ab4d7a95deda6ebf2ee3ab14ff32bb3c437
Jay Fukumoto <jayfukum@gmail.com>
2023-02-22 22:22:02 -0800

    Initial Commit

---
+++ cs125BackendTesting/cs125BackendTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  cs125BackendTestingApp.swift
+//  cs125BackendTesting
+//
+//  Created by Jay Fukumoto on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct cs125BackendTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
